8c38d78ca0d263e96535d583b14ee4e371fd043e
Paul Cooney <paul-cooney97@outlook.com>
2018-09-09 16:19:08 +0100

    Ensure file exists before attempting to Read else return empty List

--- Timezone/Reader.cs
+++ Timezone/Reader.cs
@@ -13,6 +13,11 @@ namespace Timezone
         {
             List<Tuple<string, string>> lReturn = new List<Tuple<string, string>>();
 
+            if(!File.Exists("Timezone.txt"))
+            {
+                return lReturn;
+            }
+
             string[] fileParts = File.ReadAllText("Timezone.txt").Replace("\n", "\r\n").Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
 
             foreach (string part in fileParts)
